82e6d28ae0b18b878a63f6cdb7dd2eeda8b38ea0
Rodrigo Gracas <rda.gracas@hotmail.com>
2017-11-17 22:59:14 +0000

    Handling Events

--- src/App.js
+++ src/App.js
@@ -139,6 +139,48 @@ class Clock extends React.Component {
 }
 
 
+// === Handling Events === 
+
+function ActionLink() {
+  
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
+
+  return (
+    <a href="#" onClick={handleClick}>Click me</a>
+  );
+}
+
+class Toggle extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn : true};
+  
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+    
+    // Passing Arguments to Event Handlers
+    {/* <button onClick={this.deleteRow.bind(this, id)}>Delete Row</button> */}
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -157,6 +199,8 @@ class App extends Component {
 
         <p>Clock component:</p>
         <Clock />
+
+        <Toggle />
       </div>
     );
   }
